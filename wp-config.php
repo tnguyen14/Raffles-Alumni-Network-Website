c2a9ee215c1fe619baf9a57cbb877c8a45c2d4b3
Tri Nguyen <tri@tridnguyen.com>
2012-09-30 20:16:48 -0400

    add child theme
    twentytwelve-child/style.css
    
    Signed-off-by: Tri Nguyen <tri@tridnguyen.com>

--- wp-config.php
+++ wp-config.php
@@ -15,17 +15,25 @@
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'raffles');
+if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {  
+    include( dirname( __FILE__ ) . '/local-config.php' );  
+}
+elseif ( file_exists( dirname( __FILE__ ) . '/sp-config.php' ) ) {  
+    include( dirname( __FILE__ ) . '/sp-config.php' );  
+}  
+else {
+    /** The name of the database for WordPress */
+    define('DB_NAME', 'tridn_rafflesians');
 
-/** MySQL database username */
-define('DB_USER', 'root');
+    /** MySQL database username */
+    define('DB_USER', 'tridn_rafflesian');
 
-/** MySQL database password */
-define('DB_PASSWORD', 'root');
+    /** MySQL database password */
+    define('DB_PASSWORD', 'duytri5290');
 
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
+    /** MySQL hostname */
+    define('DB_HOST', 'localhost');
+} 
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
