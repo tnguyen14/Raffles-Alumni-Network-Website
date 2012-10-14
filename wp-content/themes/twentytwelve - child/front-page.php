64e17e4e584f6ecf0dbb0da07de179d3e8337380
Tri Nguyen <tri@tridnguyen.com>
2012-10-14 15:51:15 -0400

    add scss, updated footer

--- wp-content/themes/twentytwelve - child/front-page.php
+++ wp-content/themes/twentytwelve - child/front-page.php
@@ -11,16 +11,17 @@ get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="entry-header">
+						<h1 class="entry-title"></h1>
+					</header>
 
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<header class="entry-header">
-					<h1 class="entry-title"></h1>
-				</header>
-
-				<div class="entry-content">
-					<?php the_content(); ?>
-				</div><!-- .entry-content -->
-			</article><!-- #post -->
+					<div class="entry-content">
+						<?php the_content(); ?>
+					</div><!-- .entry-content -->
+				</article><!-- #post -->
+			<?php endwhile; // end of the loop ?>
 
 
 		</div><!-- #content -->
--- wp-content/themes/twentytwelve - child/functions.php
+++ wp-content/themes/twentytwelve - child/functions.php
@@ -1,10 +1,15 @@
 <?php
 
 function child_scripts_styles() {
+	// register child theme informational style sheet
+	wp_register_style('child-theme', get_template_directory_uri().'/style.css', array(), '', 'screen');
 	// register child theme main style sheet
-	wp_register_style('child-main', get_template_directory_uri().'/style.css', array(), '', 'screen');
+	wp_register_style('child-main', get_template_directory_uri().'css/style.css', array(), '', 'screen');
 
+	// enqueue 'em all!
+	wp_enqueue_style('child-theme');
 	wp_enqueue_style('child-main');
+
 }
 
 add_action( 'wp_enqueue_scripts', 'child_scripts_styles');
