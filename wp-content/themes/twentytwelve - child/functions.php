e12cc840f1fd02cf52da414e7bb5b68301aabc3f
Tri Nguyen <tri@tridnguyen.com>
2012-10-14 14:56:33 -0400

    removed unwanted ')'

--- wp-content/themes/twentytwelve - child/functions.php
+++ wp-content/themes/twentytwelve - child/functions.php
@@ -2,7 +2,7 @@
 
 function child_scripts_styles() {
 	// register child theme main style sheet
-	wp_register_style('child-main', get_template_directory_uri().'/style.css'), array(), '', 'screen');
+	wp_register_style('child-main', get_template_directory_uri().'/style.css', array(), '', 'screen');
 
 	wp_enqueue_style('child-main');
 }
